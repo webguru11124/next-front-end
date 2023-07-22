dc8731cb48b5961f130023a21ab2eaa0037dc4df
somecoder123 <135623212+somecoder123@users.noreply.github.com>
2023-07-22 15:20:39 +0300

    Create index.d.ts

---
+++ src/app/(types)/index.d.ts
@@ -0,0 +1,16 @@
+export interface ILanguageOptions {
+    code: string;
+    image: any;
+    id: string;
+}
+export interface ILinks {
+    id: string;
+    linkName: string;
+    linkUrl: string;
+}
+export interface ISocialLinks {
+    id: string;
+    linkName:string;
+    linkUrl:string;
+    linkIcon: any;
+}
