1bc5fdb5318a3d5d56db16f7f368aa50a724b38f
webguru11124 <lucas.eriksson.pro@gmail.com>
2023-08-16 18:13:53 +0900

    refactor: env for next.config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   env: {
-    api_link: ***********,
-    public_link:***********,
-    current_domain:*******,
+    NEXTAUTH_SECRET: "my_ultra_secure_nextauth_secret",
+    NEXTAUTH_URL: "http://localhost:3000",
+    API_URL: "http://localhost:4000/api/v1",
+    LOGIN_URL: "http://localhost:4000/api/v1/users/login",
+    PUBLIC_LINK: "/",
+    CURRENT_DOMAIN: "",
+    // api_link: ***********,
+    // public_link:***********,
+    // current_domain:*******,
   },
 };
 
