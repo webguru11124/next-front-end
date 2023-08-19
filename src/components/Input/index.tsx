c81e848b20d747ba293891ff38f251be25d3e722
somecoder123 <135623212+somecoder123@users.noreply.github.com>
2023-08-19 10:59:57 +0300

    Update index.tsx

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cx, css } from '@emotion/css';
+import { cx, css } from "@emotion/css";
 import React, { InputHTMLAttributes, useState } from "react";
 import {
   CompanyEmailSvg,
@@ -8,29 +8,49 @@ import {
   EyeSvg,
   MenuSideBarIcon,
 } from "@/assets/icons";
-import { FieldError, FieldErrorsImpl, Merge, UseFormRegister } from 'react-hook-form';
+import {
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+  UseFormRegister,
+} from "react-hook-form";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
   placeholder: string;
   label?: string;
   register?: UseFormRegister<any>;
-  error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
+  error?:
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined
+    | any;
 }
 
-const Input = ({ icon, name, placeholder, type, className, error, register, label, ...rest }: InputProps) => {
+const Input = ({
+  icon,
+  name,
+  placeholder,
+  type,
+  className,
+  error,
+  register,
+  label,
+  ...rest
+}: InputProps) => {
   return (
     <div>
       {label && <label>{label}</label>}
       <div className="relative mt-2">
-        {icon && <div className="absolute inset-y-0 px-3 py-3">
-          {icon}
-        </div>}
+        {icon && <div className="absolute inset-y-0 px-3 py-3">{icon}</div>}
         <input
           {...(register && name ? register(name) : {})}
           type={type ?? "text"}
           name={name}
-          className={cx(`w-full rounded-md border border-gray-border outline-none py-2 text-lg  ${className}`, { "px-[3em]": !!icon, "px-[1em]": !!!icon })}
+          className={cx(
+            `w-full rounded-md border border-gray-border outline-none py-2 text-lg  ${className}`,
+            { "px-[3em]": !!icon, "px-[1em]": !!!icon },
+          )}
           placeholder={placeholder}
           {...rest}
         />
@@ -39,7 +59,7 @@ const Input = ({ icon, name, placeholder, type, className, error, register, labe
             <EyeSvg />
           </div>
         )}
-        {error && <span className='text-red'>{error.message}</span>}
+        {error && <span className="text-red">{error.message}</span>}
       </div>
     </div>
   );
