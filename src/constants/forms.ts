6e7c33d2de3ee925047630ad7a111dc145e7b017
webguru11124 <lucas.eriksson.pro@gmail.com>
2023-08-20 08:13:59 +0900

    refactor: update some constants

---
+++ src/constants/forms.ts
@@ -0,0 +1,4 @@
+export const Genders = ["male", "female"];
+export const Countries = ["England", "US", "India", "Pakistan", "Sweden", "France"];
+export const Languages = ["English", "French", "Germany"]
+export const Timezones = ["CET", "UDT", "ECT"]
