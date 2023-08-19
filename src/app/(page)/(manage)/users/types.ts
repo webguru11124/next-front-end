0b0cbd39e91b9bff87cad8449ecd24a757697869
somecoder123 <135623212+somecoder123@users.noreply.github.com>
2023-08-19 11:01:28 +0300

    Update types.ts

--- src/app/(page)/(manage)/users/types.ts
+++ src/app/(page)/(manage)/users/types.ts
@@ -1,10 +1,10 @@
-
 export interface User {
-    id: string,
-    avatar?: string,
-    name?: string,
-    f_name: string,
-    l_name?: string,
-    email: string,
-    role?: string,
+  id: string;
+  avatar?: string;
+  name?: string;
+  f_name?: string;
+  l_name?: string;
+  number?: string;
+  email: string;
+  role?: string;
 }
