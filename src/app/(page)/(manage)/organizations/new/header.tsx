f943d0be90dc3ef89adbaaf72ea6c7bd28cf7333
webguru11124 <lucas.eriksson.pro@gmail.com>
2023-08-20 13:09:13 +0900

    feat: organization create

--- src/app/(page)/(manage)/organizations/new/header.tsx
+++ src/app/(page)/(manage)/organizations/new/header.tsx
@@ -1,14 +1,15 @@
+"use client"
 
-
+import useGetProfile from "@/api/user/useGetProfile";
 import OrganizationHeaderButtons from "../headerButtons";
 
 export default function OrganizationAllHeader() {
+    const { data } = useGetProfile();
     return <>
         <div className="flex justify-between">
             <h1 className="text-blue-main text-3xl font-bold mt-5 ">Setup Organization</h1>
-            <OrganizationHeaderButtons />
         </div >
-        <h1 className="mt-4 text-2xl text-black">Hi, Sam!</h1>
+        <h1 className="mt-4 text-2xl text-black">{`Hi, ${data?.f_name} !`}</h1>
         <div className="text-white-darker">Add the necessary details about the organization to get started.</div>
     </>
 }
--- src/app/(page)/(manage)/organizations/new/page.tsx
+++ src/app/(page)/(manage)/organizations/new/page.tsx
@@ -1,131 +1,129 @@
+"use client"
 
+import useOrganizationCreate from "@/api/organization/useOrganizationCreate";
 import Card from "@/components/Card";
 import Input from "@/components/Input";
 import SelectBox from "@/components/SelectBox";
-
-
-const COMPLETION_STATUSES = [
-    {
-        value: 'male',
-        label: 'male',
-    },
-    {
-        value: 'female',
-        label: 'female',
-    },
-];
+import { Countries, Currencies, Genders, Languages, OrgTypes, Province, Roles, Timezones } from "@/constants/forms";
+import { OrgForm, OrgSchema } from "@/types/organization";
+import { LanguageIcon } from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import classNames from "classnames";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 export default function CreateOrganization() {
-    return (<div className="flex flex-col">
-        < div className="flex flex-col gap-y-5">
-            <Card size="lg">
-                <div className="mb-5">
-                    <h2 className="text-blue-main  font-bold text-2xl text-center">Organization Details</h2>
-                </div>
-                <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
-                    <div className="col-start-1">
-                        <Input label="Organization Name" name="name" placeholder="Enter Name here" />
+    const router = useRouter();
+    let initalData: OrgForm = {
+        invite_email: null,
+        country: null,
+        timezone: null,
+        language: null,
+        province: null,
+        type: null,
+        invite_role: null,
+        currency: null,
+        name: "",
+    };
+    const { register, handleSubmit, control, formState: { errors: formErrors, isSubmitted }, watch, reset } = useForm({
+        defaultValues: initalData,
+        resolver: zodResolver(OrgSchema),
+        mode: 'onChange',
+    });
+    const { mutate } = useOrganizationCreate();
+    const onSubmit = (data: OrgForm) => {
+        mutate(data);
+        console.log(data);
+    }
+
+    return (<div >
+        <form onSubmit={handleSubmit(onSubmit)}>
+            < div className="flex flex-col gap-y-5">
+                <Card size="lg">
+                    <div className="mb-5">
+                        <h2 className="text-blue-main  font-bold text-2xl text-center">
+                            Organization Details
+                        </h2>
                     </div>
-                    <SelectBox
-                        label="Gender"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select your gender')}`}
-                    />
-                    <div className="col-start-1">
+                    <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
+                        <Input label="Organization Name" name="name" placeholder="Enter Name here" register={register} error={formErrors.name} />
                         <SelectBox
+                            name="country"
+                            control={control}
                             label="Country"
-                            options={COMPLETION_STATUSES}
+                            options={Countries}
                             placeholder={`${('Select a country')}`}
                         />
-                    </div>
-                    <SelectBox
-                        label="Language"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select a language')}`}
-                    />
-                    <div className="col-span-2">
                         <SelectBox
-                            label="Time Zone"
+                            name="province"
+                            control={control}
+                            label="State/Province"
+                            options={Province}
+                            placeholder={`${('Select hazard')}`}
+                        />
+                        <SelectBox
+                            name="type"
+                            control={control}
+                            label="Type"
 
-                            options={COMPLETION_STATUSES}
-                            placeholder={`${('Select your timezone')}`}
+                            options={OrgTypes}
+                            placeholder={`${('Select type of organization')}`}
                         />
                     </div>
-                </div>
-            </Card>
-            <Card size="lg">
-                <div className="mb-5">
-                    <h2 className="text-blue-main  font-bold text-2xl text-center">Invite User</h2>
-                </div>
-                <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
-                    <div className="col-start-1">
-                        <Input label="Organization Name" name="name" placeholder="Enter Name here" />
+                </Card>
+                <Card size="lg">
+                    <div className="mb-5">
+                        <h2 className="text-blue-main  font-bold text-2xl text-center">
+                            Invite User</h2>
                     </div>
-                    <SelectBox
-                        label="Gender"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select your gender')}`}
-                    />
-                    <div className="col-start-1">
+                    <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
+                        <Input label="Name" name="invite_name" placeholder="Enter the name of user " register={register} />
+                        <Input label="Email" name="invite_email" placeholder="Enter the email of user" register={register} error={formErrors.invite_email} />
                         <SelectBox
-                            label="Country"
-                            options={COMPLETION_STATUSES}
-                            placeholder={`${('Select a country')}`}
+                            name="invite_role"
+                            control={control}
+                            label="Role"
+                            options={Roles}
+                            placeholder={`${('Select role of user')}`}
                         />
                     </div>
-                    <SelectBox
-                        label="Language"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select a language')}`}
-                    />
-                    <div className="col-span-2">
+                </Card>
+                <Card size="lg">
+                    <div className="mb-5">
+                        <h2 className="text-blue-main  font-bold text-2xl text-center">Regional Settings </h2>
+                    </div>
+                    <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
                         <SelectBox
-                            label="Time Zone"
+                            name="currency"
+                            control={control}
+                            label="currency"
+                            options={Currencies}
+                            placeholder={`${('Select a currency')}`}
+                        />
+                        <SelectBox
+                            name="language"
+                            control={control}
+                            label="Language"
+                            options={Languages}
+                            placeholder={`${('Select a language')}`}
+                        />
+                        <div className="col-span-2">
+                            <SelectBox
+                                name="timezone"
+                                control={control}
+                                label="Time Zone"
+                                options={Timezones}
+                                placeholder={`${('Select your timezone')}`}
+                            /></div>
+                    </div>
+                </Card>
+            </div>
+            <div className='mt-9 w-full flex justify-between'>
+                <button onClick={() => router.back()} className="text-light-color"> Skip For Later</button>
+                <button className="rounded-md text-2lg bg-blue-primary py-2.5 px-10 text-white font-bold " >Get Started</button>
+            </div>
+        </form>
+    </div >
 
-                            options={COMPLETION_STATUSES}
-                            placeholder={`${('Select your timezone')}`}
-                        />
-                    </div>
-                </div>
-            </Card>
-            <Card size="lg">
-                <div className="mb-5">
-                    <h2 className="text-blue-main  font-bold text-2xl text-center">Regional Settings </h2>
-                </div>
-                <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
-                    <div className="col-start-1">
-                        <Input label="Organization Name" name="name" placeholder="Enter Name here" />
-                    </div>
-                    <SelectBox
-                        label="Gender"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select your gender')}`}
-                    />
-                    <div className="col-start-1">
-                        <SelectBox
-                            label="Country"
-                            options={COMPLETION_STATUSES}
-                            placeholder={`${('Select a country')}`}
-                        />
-                    </div>
-                    <SelectBox
-                        label="Language"
-                        options={COMPLETION_STATUSES}
-                        placeholder={`${('Select a language')}`}
-                    />
-                    <div className="col-span-2">
-                        <SelectBox
-                            label="Time Zone"
-
-                            options={COMPLETION_STATUSES}
-                            placeholder={`${('Select your timezone')}`}
-                        />
-                    </div>
-                </div>
-            </Card>
-        </div>
-        <div className='mt-9 w-full flex justify-end'>
-            <button className="rounded-md text-[18px] bg-blue-primary py-2.5 px-10 text-white font-bold " >Get Started</button>
-        </div>
-    </div>
     );
 }
