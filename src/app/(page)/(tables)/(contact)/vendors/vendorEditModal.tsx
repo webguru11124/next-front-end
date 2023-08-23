4579e1ee5a884f8ed0b70c52a385202451b2939d
webguru11124 <lucas.eriksson.pro@gmail.com>
2023-08-23 14:56:21 +0900

    fix: update vendorEditModal

--- src/app/(page)/(tables)/(contact)/vendors/vendorEditModal.tsx
+++ src/app/(page)/(tables)/(contact)/vendors/vendorEditModal.tsx
@@ -6,169 +6,114 @@ import Avatar from "@/components/Avatar";
 import Input from "@/components/Input";
 import Modal from "@/components/Modal";
 import SelectBox from "@/components/SelectBox";
-import { Currencies } from "@/constants/forms";
 import {
   ModalType,
   useClose,
   useModalType,
   useOpen,
-  useSelected,
 } from "@/store/useModalStore";
-import { Organization } from "@/types/organization";
-import { useForm } from "react-hook-form";
-import { BsCloudArrowUp } from "react-icons/bs";
 import { GrClose } from "react-icons/gr";
 
+const COMPLETION_STATUSES = [
+  {
+    value: "male",
+    label: "male",
+  },
+  {
+    value: "female",
+    label: "female",
+  },
+];
 
-
-export default function VendorEditModal() {
+export default function EditdVendorModal() {
   const close = useClose();
   const modal = useModalType();
-  const id = useSelected();
-  const { id: user_id } = useCurrentUser();
-  const { data } = useOrganizationsByUserQuery(id);
-  const Organizations = data?.map((org: Organization) => org.name) ?? [];
-  const { register, control, formState: { errors } } = useForm();
-  if (modal !== ModalType.VendorEditModal)
-    return <div></div>
-  return ((
-    <Modal width="xl" className="h-[714px] py-4">
-      <div className="flex flex-col h-full">
-        <div className="flex relative justify-center">
-          <div className="flex flex-col  justify-center">
-            <span className="font-bold  text-blue-main text-2xl">
-              {`${id ? "Edit" : "ADD New"} Vendor`}
-            </span>
-            <div className="mt-5 mb-4 flex justify-center">
-              <Avatar size="md" />
+  return (
+    modal === ModalType.VendorEditModal && (
+      <Modal width="xl" className="h-[714px] py-4">
+        <div className="flex flex-col h-full">
+          <div className="flex relative justify-center">
+            <div className="flex flex-col  mr-[50px]">
+              <span className="font-bold  text-blue-main text-2xl">
+                Edit dVendor
+              </span>
+              <div className="mt-5 mb-4">
+                <Avatar size="md" />
+              </div>
             </div>
+            <button
+              className="h-10 w-10 flex justify-center items-center rounded bg-white text-grayscale-secondary absolute right-0"
+              onClick={close}
+            >
+              <GrClose />
+            </button>
           </div>
-          <button
-            className="h-10 w-10 flex justify-center items-center rounded bg-white text-grayscale-secondary absolute right-0"
-            onClick={close}
-          >
-            <GrClose />
-          </button>
+          <form>
+            <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
+              <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
+                Personal Details
+              </div>
+              <div className="col-start-1">
+                <Input
+                  label="First Name"
+                  name="first_name"
+                  placeholder="Enter first Name"
+                />
+              </div>
+              <Input
+                label="Last Name"
+                name="last_name"
+                placeholder="Enter last Name"
+              />
+              <Input
+                label="Email"
+                name="email"
+                placeholder="Enter email here"
+              />
+              <Input
+                label="Phone"
+                name="phone"
+                placeholder="Enter phone here"
+              />
+              {/* <SelectBox
+                        label="Gender"
+                        options={COMPLETION_STATUSES}
+                        placeholder={`${('Select your gender')}`}
+                    />
+                    <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
+                        Regional Settings
+                    </div>
+                    <div className="col-start-1">
+                        <SelectBox
+                            label="Country"
+                            options={COMPLETION_STATUSES}
+                            placeholder={`${('Select a country')}`}
+                        />
+                    </div>
+                    <SelectBox
+                        label="Language"
+                        options={COMPLETION_STATUSES}
+                        placeholder={`${('Select a language')}`}
+                    />
+                    <div className="col-span-2">
+                        <SelectBox
+                            label="Time Zone"
+                            options={COMPLETION_STATUSES}
+                            placeholder={`${('Select your timezone')}`}
+                        />
+                    </div> */}
+            </div>
+            <div className="py-10 flex justify-center">
+              <button className="rounded-md text-[18px] bg-blue-primary py-2.5 px-7 text-white mr-12 font-bold">
+                Save
+              </button>
+              <button className="rounded-md text-[18px] border-2 border-red py-2.5 px-7 text-red  font-bold">
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
-        <form>
-          <div className="grid grid-cols-2 gap-x-14  gap-y-6 px-6  ">
-            <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
-              Vendor Details
-            </div>
-            <div className="col-start-1">
-              <Input
-                label="Name"
-                name="name"
-                placeholder="Enter name here"
-                register={register}
-                error={errors.name}
-              />
-            </div>
-            <Input
-              label="Email"
-              name="email"
-              register={register}
-              error={errors.email}
-              placeholder="Enter email here"
-            />
-            <Input
-              label="Phone"
-              name="phone"
-              register={register}
-              error={errors.phone}
-              placeholder="Enter phone here"
-            />
-            <Input
-              label="Website"
-              name="website"
-              register={register}
-              error={errors.website}
-              placeholder="Paste url"
-            />
-            <SelectBox
-              label="Currency"
-              name="currency"
-              options={Currencies}
-              control={control}
-              placeholder={`${('Enter Currency')}`}
-            />
-            <SelectBox
-              label="Organization"
-              name="currency"
-              options={Organizations}
-              control={control}
-              placeholder={`${('Select an organization')}`}
-            />
-            <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
-              Vendor Registration
-            </div>
-            <div className="col-start-1">
-              <Input
-                register={register}
-                name="reg_number"
-                label="Reg Number"
-                placeholder={`${('Enter registration number')}`}
-              />
-            </div>
-            <Input
-              register={register}
-              name="reg_document"
-              righticon={<BsCloudArrowUp />}
-              label="Reg Documents"
-              placeholder={`${('Click to upload')}`}
-            />
-            <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
-              Vendor Address
-            </div>
-            <div className="col-start-1">
-              <Input
-                register={register}
-                name="billing_address"
-                label="Billing Address"
-                placeholder={`${('Enter registration number')}`}
-              />
-            </div>
-            <Input
-              register={register}
-              name="shipping_address"
-              label="Shipping Address"
-              placeholder={`${('Enter shipping address')}`}
-            />
-            <div className="col-start-1 bg-gray-max-light text-xl py-2 mt-5 text-blue-main ml-[-48px] px-12">
-              Extra Fields
-            </div>
-            <div className="col-start-1">
-              <Input
-                register={register}
-                name="billing_address"
-                label="Billing Address"
-                placeholder={`${('Enter registration number')}`}
-              />
-            </div>
-            {/* <SelectBox
-              label="Language"
-              options={COMPLETION_STATUSES}
-              placeholder={`${('Select a language')}`}
-            />
-            <div className="col-span-2">
-              <SelectBox
-                label="Time Zone"
-                options={COMPLETION_STATUSES}
-                placeholder={`${('Select your timezone')}`}
-              />
-            </div> */}
-          </div>
-          <div className="py-10 flex justify-center">
-            <button className="rounded-md text-[18px] bg-blue-primary py-2.5 px-7 text-white mr-12 font-bold">
-              Save
-            </button>
-            <button className="rounded-md text-[18px] border-2 border-red py-2.5 px-7 text-red  font-bold">
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
-    </Modal>
-  )
+      </Modal>
+    )
   );
 }
